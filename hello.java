823ccf869c1479ab61fcec9196b8a893e6d6eba1
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-01-10 17:29:26 +0000

    --- Auto-Git Commit ---

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
 public class hello {
-    public static void name(Str) {
+/**
+     *
+     */
+    private static final String HELLO_WORLD = "hello World";
+
+public static void main(String[] args) {
+    System.out.println(HELLO_WORLD);
+}
+
         
     }
-}
+    
+
